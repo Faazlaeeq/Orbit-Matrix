3f627df0f259abae7436029a2a4e646ee55816f7
Faaz Laeeq <faazlaeeq@gmail.com>
2025-10-22 17:08:17 +0500

    more fixes 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { seoKeywords } from "./seoKeywords";
 
 export const metadata: Metadata = {
   // metadataBase: new URL(process.env.NEXT_PUBLIC_LIVE_URL as string),
